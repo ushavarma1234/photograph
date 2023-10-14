62137ec0ed25b7c96d3460e25d420d059625655d
ushavarma1234 <129169013+ushavarma1234@users.noreply.github.com>
2023-10-14 10:47:57 +0530

    usha-photography

---
+++ photography/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { createTables } = require("./connection"); // Import the createTables function from connection.js
+const app = express();
+const cors = require('cors');
+
+const appointmentRoutes = require('./appoinment');  // Correct the filename to 'appointment.js'
+const commentRoutes = require('./comment');
+const contactusRoutes = require('./contactus');
+const pricingRoutes = require('./pricing');
+const storiesRoutes = require('./stories');
+const photostories = require('./photosss');
+const imagesRoutes= require('./images')
+const mysql = require("mysql2");
+
+
+const connection = mysql.createConnection({
+  host: "database-2.ce8foznoiqpc.ap-south-1.rds.amazonaws.com",
+  user: "admin", // Replace with your MySQL username
+  password: "T84qUhL1PMbujNsBmnDx", // Replace with your MySQL password
+  database: "photo",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL");
+    // No need to call createTables() here, as we will call it once before starting the server
+    startServer();
+  }
+});
+
+function startServer() {
+  createTables(); // Call the createTables function here to create the tables before starting the server
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+  
+  // Use the routes from the separate files
+  app.use('/api/appointment', appointmentRoutes);  // Correct the route path to '/api/appointment'
+  app.use('/api/comment', commentRoutes);
+  app.use('/api/contactus', contactusRoutes);
+  app.use('/api/pricing', pricingRoutes);
+  app.use('/api/stories', storiesRoutes);
+  app.use('/api/photos', photostories)
+app.use('/api/images',imagesRoutes)
+
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
---
+++ photography/pricing.js
@@ -0,0 +1,169 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql2");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
+
+const connection = mysql.createConnection({
+  host: "database-2.ce8foznoiqpc.ap-south-1.rds.amazonaws.com",
+  user: "admin", // Replace with your MySQL username
+  password: "T84qUhL1PMbujNsBmnDx", // Replace with your MySQL password
+  database: "photo",
+});
+
+
+router.use(cors());
+
+
+
+// API endpoint to get all pricing entries
+router.get("/pricing", (req, res) => {
+  const sql = "SELECT * FROM pricing";
+  connection.query(sql, async (err, results) => {
+    if (err) {
+      console.error("Error fetching pricing data:", err);
+      res.status(500).json({ error: "Error fetching pricing data" });
+    } else {
+      try {
+        // Read the image files and convert them to base64
+        for (const result of results) {
+          const imagePath = result.image_path;
+          if (imagePath) {
+            const base64Image = await readFileAsBase64(imagePath);
+            result.image = base64Image;
+          }
+        }
+        res.json(results);
+      } catch (error) {
+        console.error("Error reading image:", error);
+        res.status(500).json({ error: "Error fetching pricing data" });
+      }
+    }
+  });
+});
+
+// Function to read a file and convert it to base64
+const readFileAsBase64 = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, { encoding: "base64" }, (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+
+
+// API endpoint to create a new pricing entry
+router.post("/pricing", upload.single("image"), (req, res) => {
+  const { title, subtitle, terms_and_conditions, price } = req.body;
+  const imageFile = req.file; // Access the uploaded image file here
+
+  if (!imageFile || !title || !subtitle || !terms_and_conditions || !price) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  // Check if the maximum limit of 6 rows is reached
+  const checkQuery = "SELECT COUNT(*) AS rowCount FROM pricing";
+  connection.query(checkQuery, (err, results) => {
+    if (err) {
+      console.error("Error checking row count:", err);
+      return res.status(500).json({ error: "Error checking row count" });
+    }
+
+    const rowCount = results[0].rowCount;
+
+    if (rowCount >= 6) {
+      return res.status(400).json({ error: "Maximum limit of 6 rows reached" });
+    }
+
+    // If the limit is not reached, insert the new pricing data
+    const insertQuery = `INSERT INTO pricing (image, title, subtitle, terms_and_conditions, price)
+      VALUES (?, ?, ?, ?, ?)`;
+
+    connection.query(
+      insertQuery,
+      [imageFile.buffer, title, subtitle, terms_and_conditions, price],
+      (err, results) => {
+        if (err) {
+          console.error("Error creating pricing entry:", err);
+          res.status(500).json({ error: "Error creating pricing entry" });
+        } else {
+          res.status(201).json({ id: results.insertId, ...req.body });
+        }
+      }
+    );
+  });
+});
+
+
+
+// API endpoint to update an existing pricing entry
+router.put("/pricing/:id", (req, res) => {
+  const id = req.params.id;
+  const { image, title, subtitle, terms_and_conditions } = req.body;
+  const sql =
+    "UPDATE pricing SET image = ?, title = ?, subtitle = ?, terms_and_conditions = ? WHERE id = ?";
+  connection.query(
+    sql,
+    [image, title, subtitle, terms_and_conditions, id],
+    (err) => {
+      if (err) {
+        console.error("Error updating pricing entry:", err);
+        res.status(500).json({ error: "Error updating pricing entry" });
+      } else {
+        res.json({ id, ...req.body });
+      }
+    }
+  );
+});
+
+// API endpoint to delete a pricing entry
+router.delete("/pricing/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM pricing WHERE id = ?";
+  connection.query(sql, [id], (err) => {
+    if (err) {
+      console.error("Error deleting pricing entry:", err);
+      res.status(500).json({ error: "Error deleting pricing entry" });
+    } else {
+      res.json({ message: "Pricing entry deleted successfully" });
+    }
+  });
+});
+
+
+// API endpoint to get a single pricing entry by its ID
+router.get("/pricings/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM pricing WHERE id = ?";
+  connection.query(sql, [id], async (err, results) => {
+    if (err) {
+      console.error("Error fetching pricing data:", err);
+      res.status(500).json({ error: "Error fetching pricing data" });
+    } else if (results.length === 0) {
+      res.status(404).json({ error: "Pricing entry not found" });
+    } else {
+      try {
+        // Assuming image_path column is used to store the file path in the database
+        const imagePath = results[0].image_path;
+        if (imagePath) {
+          const base64Image = await readFileAsBase64(imagePath);
+          results[0].image = base64Image;
+        }
+        res.status(200).json(results[0]);
+      } catch (error) {
+        console.error("Error reading image:", error);
+        res.status(500).json({ error: "Error fetching pricing data" });
+      }
+    }
+  });
+});
+
+module.exports = router;
